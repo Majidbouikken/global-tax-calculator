6922b2e066cdecce9ad85cc755b648cc592f27d5
Majidbouikken <bouikkenmajid@gmail.com>
2024-08-02 11:01:10 +0300

    feat: initialize project with Spring Intlzr

---
+++ src/main/java/com/odealim/global_tax_calculator/GlobalTaxCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.odealim.global_tax_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalTaxCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalTaxCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odealim/global_tax_calculator/GlobalTaxCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odealim.global_tax_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalTaxCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
