6115495ecf107331431e2c26e8fcc0519f6b6da6
Majidbouikken <bouikkenmajid@gmail.com>
2024-08-02 12:18:36 +0300

    feat: add product Post and Get methods, configure security

--- src/main/java/com/odealim/global_tax_calculator/domain/model/Product.java
+++ src/main/java/com/odealim/global_tax_calculator/domain/model/Product.java
@@ -1,6 +1,9 @@
 package com.odealim.global_tax_calculator.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -11,8 +14,15 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @NotBlank(message = "Le nom est obligatoirey")
     String name;
+
+    @NotNull(message = "Le prix est obligatoire")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Le prix doit être supérieur à zéro")
     BigDecimal price;
+
+    @NotNull(message = "Le pays est obligatoire")
     @Enumerated(EnumType.STRING)
     Country country;
 }
